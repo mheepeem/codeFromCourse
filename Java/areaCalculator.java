2183fa2452184eadeee2edff0dc1610ac572316a
Peem <108571684+mheepeem@users.noreply.github.com>
2023-01-31 20:13:59 +0700

    Create areaCalculator.java

---
+++ Java/areaCalculator.java
@@ -0,0 +1,27 @@
+/*
+Write a method named area with one double parameter named radius.
+
+The method needs to return a double value that represents the area of a circle.
+
+If the parameter radius is negative then return -1.0 to represent an invalid value.
+
+Write another overloaded method with 2 parameters x and y (both doubles), where x and y represent the sides of a rectangle.
+
+The method needs to return an area of a rectangle.
+
+If either or both parameters is/are a negative return -1.0 to indicate an invalid value.
+*/
+
+public class AreaCalculator {
+    public static double area(double redius){
+        if(redius < 0)
+            return -1.0;
+        return Math.PI*(redius*redius);
+    }
+
+    public static double area(double x, double y){
+        if((x < 0) || (y < 0))
+            return -1.0;
+        return x*y;
+    } 
+}
